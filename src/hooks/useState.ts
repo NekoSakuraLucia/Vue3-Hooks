5fd069ea4f5f8ce321e35d72390bb8a9fe1b4e5c
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-23 19:11:24 +0700

    Add the useState code along with the function comments.

--- src/hooks/useState.ts
+++ src/hooks/useState.ts
@@ -1,3 +1,23 @@
-export function useState() {
-    console.log("useState Hooks")
+import { ref } from "vue";
+
+/**
+ * A custom hook that manages state in a Vue component.
+ * This hook provides a reactive state and a setter function to update the state.
+ *
+ * @param initialState The initial value of the state.
+ * @returns An array containing the reactive state and a function to update the state.
+ * 
+ * The returned array is a tuple where:
+ * - The first element is the `state`, a reactive reference to the current value.
+ * - The second element is `setState`, a function that updates the `state` value.
+ */
+export function useState<T>(initiaState: T) {
+    const state = ref<T>(initiaState);
+    const setState = (newState: T | ((prev: T) => T)) => {
+        state.value = typeof newState === "function"
+            ? (newState as (prev: T) => T)(state.value)
+            : newState;
+    };
+
+    return [state, setState] as const;
 }
