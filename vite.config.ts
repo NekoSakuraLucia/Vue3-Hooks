79138014842cb2ff5df1960478f3658279ebe624
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-23 18:44:24 +0700

    init tsconfig and vite config

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueHooksPackage',
+      fileName: (format) => `vue-hooks-package.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
